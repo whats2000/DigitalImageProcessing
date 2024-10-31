ae077bd79c9c7d82e70ad151e6b55df74c51be9e
whats2000 <60466660+whats2000@users.noreply.github.com>
2024-10-31 20:30:15 +0800

    Add: Add the buttons for 2nd question

--- Homework2/image_operations_hw2.py
+++ Homework2/image_operations_hw2.py
@@ -9,43 +9,50 @@ from image_processor_core import ImageProcessorCore
 if TYPE_CHECKING:
     from app import ImageProcessorApp
 
+
 class ImageOperationsHW2(ImageOperationsHW1):
     def __init__(self, app: 'ImageProcessorApp'):
         super().__init__(app)
         self.app = app
 
-    def apply_averaging_mask(self):
+    def apply_averaging_mask(self, mask_size: int = 3):
         """
-        Apply a 3x3 averaging mask to both the main and comparison images using OpenCV.
+        Apply a averaging mask to both the main and comparison images using OpenCV.
+
+        Args:
+            mask_size (int): The size of the averaging mask
         """
         if not self.app.image:
             messagebox.showinfo("Info", "Please open an image first")
             return
 
-        main_filtered_image = ImageProcessorCore.apply_average_mask(self.app.image, 3)
+        main_filtered_image = ImageProcessorCore.apply_average_mask(self.app.image, mask_size)
 
         if not self.app.compare_image:
             self.app.update_image([main_filtered_image, None])
             return
 
-        compare_filtered_image = ImageProcessorCore.apply_average_mask(self.app.compare_image, 3)
+        compare_filtered_image = ImageProcessorCore.apply_average_mask(self.app.compare_image, mask_size)
         self.app.update_image([main_filtered_image, compare_filtered_image])
 
-    def apply_median_mask(self):
+    def apply_median_mask(self, mask_size: int = 3):
         """
         Apply a 3x3 median filter to both the main and comparison images using OpenCV.
+
+        Args:
+            mask_size (int): The size of the median mask
         """
         if not self.app.image:
             messagebox.showinfo("Info", "Please open an image first")
             return
 
-        main_filtered_image = ImageProcessorCore.apply_median_mask(self.app.image, 3)
+        main_filtered_image = ImageProcessorCore.apply_median_mask(self.app.image, mask_size)
 
         if not self.app.compare_image:
             self.app.update_image([main_filtered_image, None])
             return
 
-        compare_filtered_image = ImageProcessorCore.apply_median_mask(self.app.compare_image, 3)
+        compare_filtered_image = ImageProcessorCore.apply_median_mask(self.app.compare_image, mask_size)
         self.app.update_image([main_filtered_image, compare_filtered_image])
 
     def apply_laplacian_mask(self):
@@ -65,3 +72,14 @@ class ImageOperationsHW2(ImageOperationsHW1):
         compare_filtered_image = ImageProcessorCore.apply_laplacian_mask(self.app.compare_image)
         self.app.update_image([main_filtered_image, compare_filtered_image])
 
+    def apply_fft(self):
+        pass
+
+    def apply_fft_magnitude_phase(self):
+        pass
+
+    def apply_fft_magnitude_only(self):
+        pass
+
+    def apply_fft_phase_only(self):
+        pass
--- Homework2/operation_panels_hw2.py
+++ Homework2/operation_panels_hw2.py
@@ -14,6 +14,8 @@ def create_hw2_panel(app: 'ImageProcessorApp', container: tk.Frame):
     panel = tk.Frame(container, bg=MAIN_THEME)
     _setup_load_save_compare_buttons(app, panel)
     _setup_mask_buttons_frame(app, panel)
+    _setup_bar_test_buttons_frame(app, panel)
+    _setup_lenna_buttons_frame(app, panel)
     return panel
 
 def _setup_load_save_compare_buttons(app: 'ImageProcessorApp', parent_frame: tk.Frame):
@@ -99,4 +101,105 @@ def  _setup_mask_buttons_frame(app: 'ImageProcessorApp', parent_frame: tk.Frame)
     app.averaging_mask_button.pack(side=tk.LEFT)
     app.median_mask_button.pack(side=tk.LEFT, padx=10)
     app.laplacian_mask_button.pack(side=tk.LEFT)
-    return button_frame
+
+"""
+2. The white bars in the test pattern shown below (BarTest.tif) are 7 pixels wide and 210
+pixels high. The separation between bars is 17 pixels. Please apply the following filters
+to this image and show the results:
+(a) A 7x7 arithmetic mean filter?
+(b) A 3x3 arithmetic mean filter?
+(c) A 7x7 median filter?
+(d) A 3x3 median filter?
+"""
+def _setup_bar_test_buttons_frame(app: 'ImageProcessorApp', parent_frame: tk.Frame):
+    """
+    Set up the frame for applying filters to the test pattern
+    """
+    # Create labels for the mask buttons
+    mask_text = tk.Label(
+        parent_frame,
+        text="Filters for test pattern",
+        bg=MAIN_THEME,
+        fg=MAIN_FONT_COLOR,
+    )
+    mask_text.pack(anchor="w", pady=5)
+
+    # Create a frame to hold the buttons
+    button_frame = tk.Frame(parent_frame, bg=MAIN_THEME, pady=10)
+    button_frame.pack(anchor="w", pady=5)
+    app.arithmetic_mean_7x7_button = tk.Button(
+        button_frame,
+        text="7x7 Arithmetic Mean",
+        command=lambda : app.operations.apply_averaging_mask(7)
+    )
+    app.arithmetic_mean_3x3_button = tk.Button(
+        button_frame,
+        text="3x3 Arithmetic Mean",
+        command=lambda : app.operations.apply_averaging_mask(3)
+    )
+    button_frame_2 = tk.Frame(parent_frame, bg=MAIN_THEME, pady=10)
+    button_frame_2.pack(anchor="w", pady=5)
+    app.median_7x7_button = tk.Button(
+        button_frame_2,
+        text="7x7 Median",
+        command=lambda : app.operations.apply_median_mask(7)
+    )
+    app.median_3x3_button = tk.Button(
+        button_frame_2,
+        text="3x3 Median",
+        command=lambda : app.operations.apply_median_mask(3)
+    )
+    app.arithmetic_mean_7x7_button.pack(side=tk.LEFT)
+    app.arithmetic_mean_3x3_button.pack(side=tk.LEFT, padx=10)
+    app.median_7x7_button.pack(side=tk.LEFT)
+    app.median_3x3_button.pack(side=tk.LEFT, padx=10)
+
+"""
+3. “Lenna” is a famous example of digital image processing. In order to have a further
+understanding of it, please do the following steps:
+(a) Obtain the 2D-FFT of the image “Lenna.tif”, and display the spectrum image of log|F(u, v)|.
+(b) Magnitude and Phase images: Do 2D-FFT to obtain the magnitude and phase of the
+image. Display its “magnitude-only image” and “phase-only image” by applying inverse 2D FFT.
+"""
+def _setup_lenna_buttons_frame(app: 'ImageProcessorApp', parent_frame: tk.Frame):
+    """
+    Set up the frame for applying filters to the test pattern
+    """
+    # Create labels for the mask buttons
+    mask_text = tk.Label(
+        parent_frame,
+        text="Filters for Lenna",
+        bg=MAIN_THEME,
+        fg=MAIN_FONT_COLOR,
+    )
+    mask_text.pack(anchor="w", pady=5)
+
+    # Create a frame to hold the buttons
+    button_frame = tk.Frame(parent_frame, bg=MAIN_THEME, pady=10)
+    button_frame.pack(anchor="w", pady=5)
+    app.fft_button = tk.Button(
+        button_frame,
+        text="2D-FFT",
+        command=app.operations.apply_fft
+    )
+    app.fft_magnitude_button = tk.Button(
+        button_frame,
+        text="Magnitude and Phase",
+        command=app.operations.apply_fft_magnitude_phase
+    )
+    button_frame_2 = tk.Frame(parent_frame, bg=MAIN_THEME, pady=10)
+    button_frame_2.pack(anchor="w", pady=5)
+    app.fft_magnitude_only_button = tk.Button(
+        button_frame_2,
+        text="Magnitude Only",
+        command=app.operations.apply_fft_magnitude_only
+    )
+    app.fft_phase_only_button = tk.Button(
+        button_frame_2,
+        text="Phase Only",
+        command=app.operations.apply_fft_phase_only
+    )
+    app.fft_button.pack(side=tk.LEFT)
+    app.fft_magnitude_button.pack(side=tk.LEFT, padx=10)
+    app.fft_magnitude_only_button.pack(side=tk.LEFT)
+    app.fft_phase_only_button.pack(side=tk.LEFT, padx=10)
